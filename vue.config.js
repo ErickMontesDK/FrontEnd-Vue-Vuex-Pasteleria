79b4062b1606e79ee350c5aa327bc0bd19e8362b
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-03-31 13:12:00 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  base:'/FrontEnd-Vue-Vuex-Pasteleria/',
   transpileDependencies: true
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/FrontEnd-Vue-Vuex-Pasteleria/'
-    : '/'
-}
+
