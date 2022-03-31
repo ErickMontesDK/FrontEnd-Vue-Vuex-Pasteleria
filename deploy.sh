9a7ca36d1c763492b81c737a0aee5a40f7bd2bb4
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-03-31 13:08:51 -0600

    new archivo para deploy de pagina

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://erickmontesdk.github.io/<REPO>
+git push -f git@github.com:ErickMontesDK/FrontEnd-Vue-Vuex-Pasteleria.git master:gh-pages
+
+cd -
