24b2eda9ed4c3bf61a978cd5f3e818d242e6470e
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-03-31 13:06:20 -0600

    feat configuracion para deploy de la pagina

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FrontEnd-Vue-Vuex-Pasteleria/'
+    : '/'
+}
