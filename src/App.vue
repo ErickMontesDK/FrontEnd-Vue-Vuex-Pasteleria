df404c2233ed78c997cc9878d75d1f0b867fffbc
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-04-01 16:06:45 -0600

    added comments

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
+  <!--Esta es la plantilla de todas las paginas
+  Los componentes aquí aparecen para todas las paginas, los cuales son-->
+  <!--Componente de header-->
   <Header/>
-  
+  <!--Router view que muestra la info de las paginas-->
   <router-view/>
+  <!--Componente de contacto-->
   <contact/>
+  <!--Componente de footer-->
   <footerd/>
   
 
 </template>
 
 <script>
+//Aqui se importan los componentes
 import Header from '@/components/header.vue'
 import contact from '@/components/contact.vue'
 import footerd from '@/components/footer.vue'
 
 
 export default {
-  name: 'HomeView',
+  name: 'app',
   components: {
+    //Componentes que se importan aqui, y se exportan a la plantilla de html index.html en carpeta public
     Header,contact,footerd
   }
 }
