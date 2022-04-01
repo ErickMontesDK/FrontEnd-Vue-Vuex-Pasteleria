94861453c915eb4234044eae594c4c8f5f6b35e2
Erick Montes <99063569+ErickMontesDK@users.noreply.github.com>
2022-04-01 03:58:25 -0600

    added comments

--- src/views/Orden.vue
+++ src/views/Orden.vue
@@ -1,14 +1,19 @@
 <template>
+<!--Aqui se muestra lo contenido en la pagina de Orden-->
 
+<!--Este es el componente "formulario" que son los forms del pedido y datos del cliente, y muestra la info antes de enviar-->
 <formulario/>
 </template>
 
 <script>
+//componentes importados
 import formulario from '@/components/formulario.vue'
 
 export default {
+  //nombre de la view
   name: 'Orden',
   components: {
+    //componentes importados y que se exportaran a App.vue
     formulario
   }
 }
